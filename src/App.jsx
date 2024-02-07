6fe1f279e98c74e87dcc1c97004db0daf8d29085
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-07 14:18:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Container } from 'react-bootstrap';
+const App = () => {
+  return (
+    <>
+
+      <header className="py-5">
+        <h1> Buscador de bebidas </h1>
+      </header>
+
+
+      <Container className='mt-5'>
+        2
+      </Container>
+    </>
+  );
+};
+
+export default App;
