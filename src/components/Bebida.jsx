bf071d016e1ee5b7da00a24bed5248ac4f1869a8
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-10 18:55:11 -0300

    agregando los card

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,6 +1,23 @@
+import { Col, Card, Button } from "react-bootstrap"
+
 
 export const Bebida = ({ bebida }) => {
     return (
-        <div>{bebida.strDrink}</div>
+        <Col md={6} lg={3}>
+            <Card className="mb-4">
+                <Card.Img
+                    variant="top"
+                    src={bebida.strDrinkThumb}
+                    alt={`Imagen de ${bebida.strDrinkThumb}`}
+                />
+            </Card>
+
+            <Card.Body>
+                <Card.Title>${bebida.strDrink}</Card.Title>
+                <Button className="w-100 text-uppercase mt-2">
+                    Ver Receta
+                </Button>
+            </Card.Body>
+        </Col>
     )
 }
