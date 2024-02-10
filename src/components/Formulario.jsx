ad21a0478c8f0a4a030b986b4b12823db09ad170
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-02-10 17:41:58 -0300

    validando formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,35 @@
-import { Button, Form, Row, Col } from "react-bootstrap"
 
+import { Button, Form, Row, Col, Alert } from "react-bootstrap"
+import { useCategorias } from "../hooks/useCategorias"
+import { useState } from "react"
 
 export const Formulario = () => {
+
+    const [alerta, setAlerta] = useState("")
+    const [busqueda, setBusqueda] = useState({
+        nombre: "",
+        categoria: ""
+    })
+
+    const { categorias } = useCategorias()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (Object.values(busqueda).includes("")) {
+            setAlerta("Todos los Campos son Requeridos")
+            return
+        }
+        setAlerta("")
+
+    }
+
     return (
-        <Form>
+        <Form
+            onSubmit={handleSubmit}
+        >
+
+            {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
             <Row>
                 <Col md={6}>
                     <Form.Group className="mb-3">
@@ -14,6 +40,12 @@ export const Formulario = () => {
                             type="text"
                             placeholder="Ej: Tequila, Vodka, etc."
                             name="nombre"
+                            value={busqueda.nombre}
+                            onChange={(e) => setBusqueda({
+                                ...busqueda,
+                                [e.target.name]: e.target.value
+
+                            })}
                         />
 
                     </Form.Group>
@@ -25,12 +57,38 @@ export const Formulario = () => {
                         <Form.Select
                             id="categoria"
                             name="categoria"
+                            value={busqueda.categoria}
+                            onChange={(e) => setBusqueda({
+                                ...busqueda,
+                                [e.target.name]: e.target.value
+
+                            })}
                         >
                             <option>-Selecciona Categoria-</option>
+                            {categorias.map(categoria => (
+                                <option
+                                    key={categoria.strCategory}
+                                    value={categoria.strCategory}
+                                >{categoria.strCategory}
+
+                                </option>
+                            ))}
 
                         </Form.Select>
 
                     </Form.Group>
+                </Col>
+            </Row>
+            <Row className="justify-content-end">
+                <Col md={3}>
+
+                    <Button
+                        variant="danger"
+                        className=" text-uppercase w-100"
+                        type="submit"
+                    >
+                        Buscar Bebidas
+                    </Button>
                 </Col>
             </Row>
         </Form>
--- src/context/CategoriasProvider.jsx
+++ src/context/CategoriasProvider.jsx
@@ -1,32 +1,36 @@
-import axios from "axios";
-import { createContext, useEffect, useState } from 'react';
+import axios from "axios"
+import { createContext, useEffect, useState } from 'react'
 
-export const CategoriasContext = createContext();
+export const CategoriasContext = createContext()
 
 export const CategoriasProvider = ({ children }) => {
 
 
+    const [categorias, setCategorias] = useState([])
+
     const obtenerCategorias = async () => {
+
+
         try {
 
-            const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
-            const { data } = await axios(url);
-            console.log(data);
+            const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+            const { data } = await axios(url)
+            setCategorias(data.drinks)
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
-    };
-    useEffect(() => {
-        obtenerCategorias();
-    }, []);
+    }
 
+    useEffect(() => {
+        obtenerCategorias()
+    }, [])
 
     return (
         <CategoriasContext.Provider value={{
-
+            categorias
         }}>
 
             {children}
         </CategoriasContext.Provider>
-    );
-};
+    )
+}
